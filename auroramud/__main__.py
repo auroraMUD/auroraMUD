1f55c77bc66ca69abbafcd306d2ebcdd91641740
blind.lightning.productions <blind.lightning.productions@gmail.com>
2023-05-08 10:07:41 +0100

    on closing the connections, saves a backup of the database

--- auroramud/__main__.py
+++ auroramud/__main__.py
@@ -6,8 +6,18 @@ port=6000
 
 def main(host, port):
     if not os.path.exists("./database/"):
-        os.makedirs("./database/")
-    
-    server.Server(host, port)
+        os.mkdir("./database/")
+    try:
+        server = server.Server(host, port)
+    except KeyboardInterrupt:
+        os.popen('cp ./database/auroramud.db ./database/auroramud.db.backup')
+        server.db.close()
+        for i in server.connections:
+            server.connections[i].disconnect()
+        server.listener.close()
+        del server
+        sys.exit()
+
+
 
 main(host, port)
