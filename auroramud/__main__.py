e7fbd45367a61c4d20a4192b2e6ec1f87b2c534c
blind.lightning.productions <blind.lightning.productions@gmail.com>
2023-05-08 10:47:13 +0100

    fix: more signal issues

--- auroramud/__main__.py
+++ auroramud/__main__.py
@@ -6,27 +6,26 @@ import server
 host='0.0.0.0'
 port=6000
 
-
+srv= None
 def main(host, port):
-    srv=None
     if not os.path.exists("./database/"):
         os.mkdir("./database/")
     
-    srv=server.Server(host, port)
+    srv = server.Server(host, port)
 
 
-    def handle_exit(signum, frame):
-        srv.send("Server is restarting, please hold\n\n\n")
-        os.popen('cp ./database/auroramud.db ./database/auroramud.db.backup')
-        srv.db.close()
-        for i in srv.connections:
-            srv.connections[i].disconnect()
-        srv.listener.close()
-        srv.selector.close()
-        del srv
-        sys.exit(0)
+def handle_exit(signum, frame):
+    srv.send("Server is restarting, please hold\n\n\n")
+    os.popen('cp ./database/auroramud.db ./database/auroramud.db.backup')
+    srv.db.close()
+    for i in srv.connections:
+        srv.connections[i].disconnect()
+    srv.listener.close()
+    srv.selector.close()
+    del srv
+    sys.exit(0)
 
 
-    signal.signal(signal.SIGTERM, handle_exit)
+signal.signal(signal.SIGTERM, handle_exit)
 
 main(host, port)
